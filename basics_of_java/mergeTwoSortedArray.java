8563ac15ce90eb707ae425859c9919578a67f186
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-11-17 18:17:47 +0530

    first commit

--- basics_of_java/mergeTwoSortedArray.java
+++ basics_of_java/mergeTwoSortedArray.java
@@ -1,5 +1,34 @@
 package basics_of_java;
 
 public class mergeTwoSortedArray {
+    public static void main(String[] args){
+        int[] arr1 = ArrayUtility.inputArray();
+        int[] arr2 = ArrayUtility.inputArray();
+        int[ ] mergeArray = mergingSortedArray(arr1,arr2);
+        System.out.print("Merged array are :");
+        for (int i = 0; i < mergeArray.length; i++) {
+            System.out.print(mergeArray[i] + " ");
+        }
+    }
+    public static int[] mergingSortedArray(int[]arr1,int[]arr2){
+        int size = arr2.length + arr1.length;
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        int i=0,j=0,k=0;
+        while(i < arr1.length || j < arr2.length){
+            if((i < arr1.length && arr1[i] < arr2[j]) || j==arr2.length){
+                mergedArray[k] = arr1[i];
+                i++;
+                k++;
+            }
+            else {
+                mergedArray[k] = arr2[j];
+                j++;
+                k++;
+            }
 
+        }
+        return mergedArray;
+
+
+    }
 }

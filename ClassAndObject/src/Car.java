fc0ce397f512f04b63b2ad4254e27d636f0dda3d
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-11-18 21:57:40 +0530

    first commit

--- ClassAndObject/src/Car.java
+++ ClassAndObject/src/Car.java
@@ -4,7 +4,22 @@ public class Car {
     float maxSpeed;
     float currentFuel;
     int noOfSeats;
-
+    static {
+        System.out.println("Static Block is called");
+    }
+    {
+        System.out.println("init block is called ");
+    }
+    Car(String color){
+        maxSpeed = 150;
+        this.color = color;
+        currentFuel = 2;
+        noOfSeats = 5;
+    }
+    Car(){
+        this("Red");
+        currentFuel = 5;
+    }
     public void drive(){
         if(currentFuel == 0){
             System.out.println("Your Car is Oud of Fuel");

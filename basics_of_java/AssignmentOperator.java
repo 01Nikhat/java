090d0dba084725bb63798ea0733902d535e6fe5d
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-01-17 20:14:39 +0530

    third commit

---
+++ basics_of_java/AssignmentOperator.java
@@ -0,0 +1,13 @@
+class AssignmentOperator {
+
+  public static void main(String[] args) {
+    int num1 = 7;
+    int num2 = 5;
+
+    int result = num1/num2; //we will get only quotient part not remainder part
+    System.out.println("result :" + result);
+
+    int remainder = num1 % num2; //we will get here only remender part
+    System.out.println("remainder : "+ remainder);
+  }
+}

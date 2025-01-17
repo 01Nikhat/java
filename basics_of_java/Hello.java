abe114901cfab4408f91fe476f20382a215ce4f0
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-01-17 18:03:00 +0530

    second commit

--- basics_of_java/Hello.java
+++ basics_of_java/Hello.java
@@ -13,12 +13,13 @@ class Hello{
 
       boolean b = true ; //never do 0 or 1 it's only take true or false value 
 
-// some interesting example
+//-------------------- some interesting example-------------------------------------------------
 
 char ch = 'a'; // here we can add on char 
 ch++; //here it will goes to plus1 means it will goes to string literals "b"
 
 System.out.println("ch :" + ch); //output : b
+
 
 int num1 = 0b1010; //this is a boolean format
 int num2 = 0x7e; //this is a hexa decimal format
@@ -32,6 +33,31 @@ System.out.println("num2 :" + num2); //output: num2 :126
 System.out.println("num3 :" + num3); //output: num3 :10000000
 
 System.out.println("result :" + result); //output: result :136
+
+//--------------------------------Type Casting-----------------------------------------------------------
+
+byte byt = 127;
+int n = byt; //implicitly the data will transfer from byte to int value
+System.out.println("n :"+ n); 
+
+int a = 257;
+byte numb = (byte)a; //we can't transfer even a small value of integer into byte  we have explicitly change int value to byte value 
+
+System.out.println("numb value :"+ numb);//the byte range is -128 to 127 the total number is 256
+//so here we have to do modulo of (257 % 256) which is 1 ,so the output is 1
+
+
+float value = 5.6f;
+int res = (int) value; //output is 5 so we loss the decimal value
+
+System.out.println("res :"+ res);
+
+//
+int largeNumber = 1_000_000;
+int resultNum = largeNumber * largeNumber; //this range will go overflow because -2^31 tp +2^31 is the range of integer so, in java we called promotion value we take the 32 bit value and represent in negative value output
+System.out.println("Result value : " + resultNum); //output : -727379968
+
+
 
   }
 }

8fde62fcff5a15ecdc531b77a2c7d3c47e48ff48
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-01-18 20:19:18 +0530

    second commit

--- basics_of_java/MethodOverLoading.java
+++ basics_of_java/MethodOverLoading.java
@@ -13,6 +13,19 @@ class Calculator {
       System.out.println("This is a default constructor used for object initialization.");
   }
 
+  /*
+     * Method Overloading:
+     * Method overloading is a feature in Java that allows multiple methods in the same class
+     * to have the same name but differ in the number or types of parameters.
+     * - It provides flexibility and improves code readability.
+     * - The return type of the method does not matter for overloading.
+     * Example: 
+     * - add(int, int)
+     * - add(int, int, int)
+     * - add()
+     * These methods all share the same name but differ in parameter lists, hence they are overloaded.
+     */
+
   // Method to add two numbers
   public int add(int n1, int n2) {
       int result = n1 + n2;
@@ -49,5 +62,15 @@ public class MethodOverLoading {
       Calculator obj3 = new Calculator(); // Calls the default constructor
       obj3.add();
       System.out.println("Third object with no arguments, treated as a method and not a constructor.");
+
+
+    //   num is tied to a specific object of the Calculator class. Each object of the class has its own copy of the instance variable.
+
+    obj.num = 5;  // Changes num for obj1
+    System.out.println("instance variable of obj object value :"+obj.num);
+    
+    obj2.num = 10; // Changes num for obj2
+    System.out.println("instance variable of obj2 object value :"+ obj2.num);
+    
   }
 }

930ddbac130ad42639deaed43add27ba45c5184c
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-01-28 00:00:29 +0530

    third commit

--- basics_of_java/ClassInsideClass.java
+++ basics_of_java/ClassInsideClass.java
@@ -1,5 +1,29 @@
+
+class Parents {
+
+  int noOfSon = 4;
+  int noOfDaughter = 1;
+
+  public void daughter(){
+      System.out.println("Good Habit of explanations ");
+  }
+
+  static class Son{
+    public void similarity(){
+      System.out.println("HardWorking person ");
+    }
+  }
+}
 public class ClassInsideClass {
   public static void main(String[] args) {
-      
+      Parents obj = new Parents();
+      obj.daughter();
+      System.out.println("Number of daughter :" + obj.noOfDaughter);
+      System.out.println("Number of Son :" + obj.noOfSon);
+
+     // obj.similarity();  we can't call parents inside class method 
+
+     Parents.Son obj1 = new Parents.Son(); //it will work only inside class is static 
+
   }
 }

1c2e714c502f184f7567127c44187cced504f2e9
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-01-28 00:06:41 +0530

    fourth comment

--- basics_of_java/ClassInsideClass.java
+++ basics_of_java/ClassInsideClass.java
@@ -1,29 +1,45 @@
-
 class Parents {
-
-  int noOfSon = 4;
-  int noOfDaughter = 1;
-
-  public void daughter(){
+  
+  int noOfSon = 4;  // Instance variable, each object of Parents will have its own copy
+  int noOfDaughter = 1;  // Instance variable
+  
+  public void daughter() {
+      // Instance method, can be called after creating an object of Parents
       System.out.println("Good Habit of explanations ");
   }
 
-  static class Son{
-    public void similarity(){
+  static class Son {
+    // Static inner class, can be instantiated without creating a Parents object
+    public void similarity() {
+      // Method of static class Son
       System.out.println("HardWorking person ");
     }
   }
 }
+
 public class ClassInsideClass {
   public static void main(String[] args) {
-      Parents obj = new Parents();
-      obj.daughter();
-      System.out.println("Number of daughter :" + obj.noOfDaughter);
-      System.out.println("Number of Son :" + obj.noOfSon);
+    // Creating an object of Parents
+    Parents obj = new Parents();
+    
+    // Calling instance method of Parents class
+    obj.daughter();
+    
+    // Accessing instance variables of Parents class
+    System.out.println("Number of daughter :" + obj.noOfDaughter);
+    System.out.println("Number of Son :" + obj.noOfSon);
 
-     // obj.similarity();  we can't call parents inside class method 
+    // You cannot call the method or access variables of the Parents class directly from the static Son class 
+    // obj.similarity(); // This will give error because similarity() is inside Son, not a method of Parents class
 
-     Parents.Son obj1 = new Parents.Son(); //it will work only inside class is static 
-
+    // Creating an instance of static inner class Son
+    // Since Son is static, we can create it without creating an object of Parents class
+    Parents.Son obj1 = new Parents.Son(); 
+    
+    // Calling the similarity() method from Son class instance
+    obj1.similarity();  // This works because Son is static and does not require a Parents object
+    
+    // You cannot create an instance of the outer class (Parents) without first creating an object
+    // of the enclosing class. This is why `new Parents().similarity()` will not work.
   }
 }
